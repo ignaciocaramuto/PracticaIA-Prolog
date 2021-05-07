a300f49e38ae4b0bf15fb745ee567abae1b8b07b
Nacho <ignaciocaramuto@gmail.com>
2021-05-07 19:07:54 -0300

    agregado ejercicio 2

---
+++ Practica 1/ejercicio2.pl
@@ -0,0 +1,11 @@
+conoce(franco,ingles).
+conoce(renzo,ingles).
+conoce(franco,frances).
+conoce(renzo,frances).
+conoce(franco,italiano).
+conoce(marco,ingles).
+conoce(omar,ingles).
+conoce(maria,frances).
+
+conoceAmbos(X):-conoce(X,ingles),conoce(X,frances).
+
